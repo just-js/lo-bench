a5dc3d0ca34c457825d66bd39c4a78b39563c2d5
Andrew Johnston <apjohnsto@gmail.com>
2023-12-11 12:30:04 +0000

    oops

--- http/https-lo.js
+++ http/https-lo.js
@@ -8,29 +8,33 @@ const { libssl } = lo.load('libssl')
 
 const { assert, utf8Length, wrap, cstr, core } = lo
 const { fcntl, O_NONBLOCK, F_SETFL } = core
+const { socket, bind, listen, accept, close, setsockopt } = net
+const { sockaddr_in } = net.types
+const { Blocked } = Loop
 const { 
-  socket, bind, listen, accept, send_string, close, setsockopt
-} = net
-const {
-  SOCK_STREAM, AF_INET, SOMAXCONN, SO_REUSEPORT, SOL_SOCKET
+  SOCK_STREAM, AF_INET, SOMAXCONN, SO_REUSEPORT, SOL_SOCKET, SOCKADDR_LEN
 } = net
 const {
   SSL_OP_ALL, SSL_OP_NO_RENEGOTIATION, SSL_OP_NO_SSLv3, SSL_OP_NO_TLSv1, 
   SSL_OP_NO_TLSv1_1, SSL_OP_NO_DTLSv1, SSL_OP_NO_DTLSv1_2, SSL_FILETYPE_PEM
 } = libssl
 const {
-  SSL_CTX_set_options, SSL_set_fd, SSL_set_accept_state, SSL_accept, 
-  SSL_get_error, SSL_CTX_use_certificate_chain_file, SSL_CTX_use_PrivateKey_file, 
-  SSL_read, SSL_write_string, SSL_free
+  SSL_CTX_set_options, SSL_set_fd, SSL_set_accept_state, SSL_accept, SSL_free,
+  SSL_CTX_use_certificate_chain_file, SSL_CTX_use_PrivateKey_file, 
+  SSL_read, SSL_write_string, SSL_get_error
 } = libssl
-const { sockaddr_in } = net.types
-const { Blocked } = Loop
-const SOCKADDR_LEN = 16
+
+function update_headers () {
+  const now = (new Date()).toUTCString()
+  plaintext = `content-type: text/plain;charset=utf-8\r\nDate: ${now}\r\n`
+}
 
 function on_timer () {
-  console.log(`rps ${stats.rps} rss ${mem()} conn ${stats.conn} sockets ${sockets.size}`)
+  const { rps, conn } = stats
+  const rss = mem()
+  console.log(`rps ${rps} rss ${rss} conn ${conn} sockets ${sockets.size}`)
   stats.rps = 0
-  plaintext = `content-type: text/plain;charset=utf-8\r\nDate: ${(new Date()).toUTCString()}\r\n`
+  update_headers()
 }
 
 function status_line (status = 200, message = 'OK') {
@@ -114,11 +118,14 @@ function create_secure_socket (fd) {
   }
 }
 
+let plaintext = ''
 const handle = new Uint32Array(2)
+const stats = { rps: 0, conn: 0 }
+const options = SSL_OP_ALL | SSL_OP_NO_RENEGOTIATION | SSL_OP_NO_SSLv3 | 
+  SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1 | SSL_OP_NO_DTLSv1 | SSL_OP_NO_DTLSv1_2
 const TLS_server_method = wrap(handle, libssl.TLS_server_method, 0)
 const SSL_CTX_new = wrap(handle, libssl.SSL_CTX_new, 1)
 const SSL_new = wrap(handle, libssl.SSL_new, 1)
-let plaintext = `content-type: text/plain;charset=utf-8\r\nDate: ${(new Date()).toUTCString()}\r\n`
 const INSECURE = 0
 const SECURE = 1
 const BUFSIZE = 16384
@@ -130,19 +137,16 @@ assert(fd > 2)
 assert(fcntl(fd, F_SETFL, O_NONBLOCK) === 0)
 assert(!setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, net.on, 32))
 assert(!bind(fd, sockaddr_in(address, port), SOCKADDR_LEN))
+update_headers()
 assert(!listen(fd, SOMAXCONN))
 const loop = new Loop()
 assert(!loop.add(fd, on_socket_connect))
-const options = SSL_OP_ALL | SSL_OP_NO_RENEGOTIATION | SSL_OP_NO_SSLv3 | 
-  SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1 | SSL_OP_NO_DTLSv1 | SSL_OP_NO_DTLSv1_2
 const method = assert(TLS_server_method())
 const ctx = assert(SSL_CTX_new(method))
 assert(SSL_CTX_use_PrivateKey_file(ctx, cstr('key.pem').ptr, SSL_FILETYPE_PEM) === 1)
 assert(SSL_CTX_use_certificate_chain_file(ctx, cstr('cert.pem').ptr) === 1)
 SSL_CTX_set_options(ctx, options)
-const stats = { rps: 0, conn: 0 }
 const timer = new Timer(loop, 1000, on_timer)
 while (loop.poll() > 0) {}
-console.log('willy')
 timer.close()
 close(fd)
